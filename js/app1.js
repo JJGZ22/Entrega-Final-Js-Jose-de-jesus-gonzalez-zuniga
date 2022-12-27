7609c16cf814f13c6c933b4f96b81905e7a4f09e
Jose de Jesus Gonzalez Zuniga <jjgz0223@gmail.com>
2022-12-27 01:15:37 -0700

    ajustes 3

--- js/app1.js
+++ js/app1.js
@@ -4,6 +4,13 @@ function suma3ordenes(valor1,valor2,valor3)
     resultado = valor1 + valor2 + valor3
     return resultado
 }
+function suma2ordenes(orden1,orden2)
+{
+    let resultado
+    resultado = orden1 + orden2
+    return resultado
+}
+
 
 let orden
 orden=parseInt(orden)
@@ -13,10 +20,8 @@ let papaprecio = 20
 papaprecio=parseInt(papaprecio)
 let bebidaprecio= 15
 bebidaprecio=parseInt(bebidaprecio)
-let ordentotal = hamburprecio+papaprecio+bebidaprecio
-ordentotal=parseInt(ordentotal)
 do{
-    orden=prompt("Bienvenido a WcDonalds! Que vas a ordenar?\n1-hamburguesa\n2-Papas\n3-Bebida\n4-Orden Completa\n5-Salir")
+    orden=prompt("Bienvenido a WcDonalds! Que vas a ordenar?\n1-hamburguesa\n2-Papas\n3-Bebida\n4-Combo hamburguesa y bebida \n5-combo hamburguesa y papas\n6-Combo Bebida y papas\n7-Combo Completo\n8-Salir")
     orden=parseInt(orden)
     switch (orden) {
         case 1:alert("Una Deliciosa Hamburguesa ala Orden! El Total es "+ hamburprecio)
@@ -25,12 +30,18 @@ do{
         break
         case 3:alert("Unas Bebida Gaseosa ala orden! El Total es "+ bebidaprecio)
         break
-        case 4:alert("Orden Completa En Camino! El Total es "+ suma3ordenes(hamburprecio,papaprecio,bebidaprecio))
+        case 4:alert("Una Hamburguesa y Bebida ala Orden! El Total es "+ suma2ordenes(hamburprecio,bebidaprecio))
         break
-        case 5:alert("Saliendo...Gracias! Vuelva Pronto")
+        case 5:alert("Una Hamburguesa Y Papas ala francesa en camino! El Total es "+ suma2ordenes(hamburprecio,papaprecio))
+        break
+        case 6:alert("Una Bebida y Unas Papas ala francesa! El Total es "+ suma2ordenes(bebidaprecio,papaprecio))
+        break
+        case 7:alert("Combo Completo En Camino! El Total es "+ suma3ordenes(hamburprecio,papaprecio,bebidaprecio))
+        break
+        case 8:alert("Saliendo...Gracias! Vuelva Pronto")
         break
      default:alert("Esa opcion no existe")
             break;
     }
-}while(orden !=5)
+}while(orden !=8)
     
