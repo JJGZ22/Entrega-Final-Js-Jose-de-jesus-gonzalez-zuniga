12dd65e35d00ee5308ef9cfccb882f076c7ec40d
Jose de Jesus Gonzalez Zuniga <jjgz0223@gmail.com>
2023-02-17 22:15:02 -0700

    Ajustes Finales

--- js/app2.js
+++ js/app2.js
@@ -1,3 +1,22 @@
+function iniciarMap(){
+    var coord = {lat:-34.5956145 ,lng: -58.4431949};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+swal({  
+  title: " Hola Gracias por Elegirnos!",  
+  text: " Bienvenido a Wcdonalds! Listo para ordenar? Te Recomendamos La Nueva Hamburguesa a la portobello es una Delicia!",  
+  imageUrl:"./img/burgericon.png",
+  imageWidth:"50px",    
+  button: "OK",  
+  
+});  
 const Clickbutton = document.querySelectorAll('.boton')
 const tbody = document.querySelector('.tbody')
 let carrito = []
@@ -140,3 +159,25 @@ window.onload = function(){
     renderCarrito()
   }
 }
+
+function myDisplayer(some) {
+  document.getElementById("slid").innerHTML = some;
+}
+
+let myPromise = new Promise(function(myResolve, myReject) {
+  let req = new XMLHttpRequest();
+  req.open('GET', "./publicidad/slider.html");
+  req.onload = function() {
+    if (req.status == 200) {
+      myResolve(req.response);
+    } else {
+      myReject("File not Found");
+    }
+  };
+  req.send();
+});
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
